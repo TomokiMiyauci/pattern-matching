2d93a29269d43995a2fb93ed27934d5a685d15bf
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-27 22:44:43 +0900

    docs(mod): add module description

--- mod.ts
+++ mod.ts
@@ -1,6 +1,12 @@
 // Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
 // This module is browser compatible.
 
+/** Syntax-free pattern matching, feature parity with TC39
+ * [proposal-pattern-matching](https://github.com/tc39/proposal-pattern-matching).
+ *
+ * @module
+ */
+
 export { match } from "./match.ts";
 export { otherwise, when, whether } from "./arm.ts";
 export { identifier, matcher, rest } from "./constants.ts";
