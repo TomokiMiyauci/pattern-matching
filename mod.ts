de08ee32653b43e4e7739569ba7e555949dd7fd5
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-27 16:07:49 +0900

    feat(mod): export all API

---
+++ mod.ts
@@ -0,0 +1,7 @@
+// Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
+export { match } from "./match.ts";
+export { otherwise, when, whether } from "./arm.ts";
+export { identifier, matcher, rest } from "./constants.ts";
+export { _ } from "./utils.ts";
